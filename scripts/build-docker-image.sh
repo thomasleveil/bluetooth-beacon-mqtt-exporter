f8bc86520a6ff55341f963c0e22c6bb19913c772
Thomas LEVEIL <thomasleveil@gmail.com>
2018-12-27 04:18:34 +0100

    improve docker build script to add schema.org labels

--- scripts/build-docker-image.sh
+++ scripts/build-docker-image.sh
@@ -10,4 +10,15 @@ fi
 DOCKER_IMAGE=tomdesinto/bluetooth-beacon-mqtt-exporter:rpi
 
 
-docker build -t ${DOCKER_IMAGE} ${DIR}/..
+docker build \
+    -t ${DOCKER_IMAGE} \
+    --label org.label-schema.build-date="$(date -u +'%Y-%m-%dT%H:%M:%SZ')" \
+    --label org.label-schema.name="bluetooth-beacon-mqtt-exporter" \
+    --label org.label-schema.description="service publishing Bluetooth BLE beacons advertisements on a mqtt topic" \
+    --label org.label-schema.usage="https://github.com/thomasleveil/bluetooth-beacon-mqtt-exporter#readme" \
+    --label org.label-schema.url="https://github.com/thomasleveil/bluetooth-beacon-mqtt-exporter" \
+    --label org.label-schema.vcs-url="https://github.com/thomasleveil/bluetooth-beacon-mqtt-exporter.git" \
+    --label org.label-schema.vcs-ref="$(git describe --always --dirty | sed 's/^v//')" \
+    --label org.label-schema.version="$(git describe --always --tags --dirty | sed 's/^v//')" \
+    --label org.label-schema.schema-version="1.0" \
+    ${DIR}/..
